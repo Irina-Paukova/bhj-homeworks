0158a1e12903c20414f095a2f95dbf13c0f593fd
Paukova Irina <i.paukova@mail.ru>
2021-06-15 11:36:30 +0300

    all tasks v.1.0

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
+    document.addEventListener('keydown', event => {
+      this.currentSymbol.innerHTML === event.key.toLowerCase() ? this.success() : this.fail();
+    });
+  };
+
+
+ 
 
   success() {
     this.currentSymbol.classList.add('symbol_correct');
